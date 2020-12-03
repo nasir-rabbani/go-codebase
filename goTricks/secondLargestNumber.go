4453980be9f57b5264c48b48603a2ae5ef9bc9d3
Nasir Rabbani <nasirr@mkcl.org>
2020-12-04 00:28:12 +0530

    handeled boundary conditions

--- goTricks/secondLargestNumber.go
+++ goTricks/secondLargestNumber.go
@@ -3,16 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	s := []int{20, 1990, 12, 1110, 1, 59, 12, 15, 120, 1110, -1, -19, 3000}
-
+	// s := []int{20, 1990, 12, 1110, 1, 59, 12, 15, 120, 1110, -1, -19, 3000}
+	s := []int{-2, 20, -3}
 	max := s[0]
-	nextToMax := s[0]
+	var nextToMax int
 
 	for _, val := range s {
 		if val > max {
+			nextToMax = max
 			max = val
 		} else if val > nextToMax {
-			nextToMax = max
+			nextToMax = val
+		} else if max == nextToMax {
+			nextToMax = val
 		}
 	}
 	fmt.Println(max)
